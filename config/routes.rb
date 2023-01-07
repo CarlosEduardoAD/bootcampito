10d21918c904351128c51541d97173f189990e87
CarlosEduardoAD <karl.devcontato@gmail.com>
2023-01-07 15:40:14 -0300

    init

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'profiles/celso'
+  delete 'profiles/:id', to: "profiles#destroy"
 
   root "home#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
